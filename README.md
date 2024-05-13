425550af5f9adc920aac85a4ccf30bd6b0459ecb
Vladimir Fibe <motiw@icloud.com>
2024-05-13 18:26:46 +0500

    resources

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-#  <#Title#>
+#  StepTracker
 
+- [HealthKit](https://developer.apple.com/design/human-interface-guidelines/healthkit)
+- [Apple Design Resources](https://developer.apple.com/design/resources/#technologies)
---
+++ StepTracker/HealthKitPermissionPrimingView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct HealthKitPermissionPrimingView: View {
+    var body: some View {
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+#Preview {
+    HealthKitPermissionPrimingView()
+}
