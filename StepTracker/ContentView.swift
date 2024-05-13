86fa780af39369a61701ea1ec368e8ca44d2a1c2
Vladimir Fibe <motiw@icloud.com>
2024-05-13 09:26:18 +0500

    First UI

--- StepTracker/ContentView.swift
+++ StepTracker/ContentView.swift
@@ -2,13 +2,55 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ScrollView {
+                VStack(spacing: 20) {
+                    VStack(spacing: 20) {
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Label("Steps", systemImage: "figure.walk")
+                                    .font(.title3.bold())
+                                .foregroundStyle(.pink)
+                                Text("Avg: 10K Steps")
+                            }
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                                .foregroundStyle(.secondary)
+                        }
+                        
+                        RoundedRectangle(cornerRadius: 12)
+                            .foregroundStyle(.secondary)
+                            .frame(height: 150)
+                    }
+                    .padding()
+                    .background(
+                        RoundedRectangle(cornerRadius: 12)
+                        .fill(Color(.secondarySystemBackground))
+                    )
+                    
+                    VStack(alignment: .leading, spacing: 20) {
+                            VStack(alignment: .leading) {
+                                Label("Averages", systemImage: "calendar")
+                                    .font(.title3.bold())
+                                .foregroundStyle(.pink)
+                                Text("Last 28 days")
+                            }
+
+                        
+                        RoundedRectangle(cornerRadius: 12)
+                            .foregroundStyle(.secondary)
+                            .frame(height: 240)
+                    }
+                    .padding()
+                    .background(
+                        RoundedRectangle(cornerRadius: 12)
+                        .fill(Color(.secondarySystemBackground))
+                    )
+                }
+            }
+            .padding()
+            .navigationTitle("Dashboard")
         }
-        .padding()
     }
 }
 
